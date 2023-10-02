5ba3f6d5d8f3447087ebb4763869f63ed8003df7
wahyu2078 <wahyutptp20@gmail.com>
2023-10-02 10:47:41 +0700

    modifikasi_percobaan1

--- PemilihanPercobaan129.java
+++ PemilihanPercobaan129.java
@@ -6,11 +6,13 @@ public class PemilihanPercobaan129 {
     Scanner input29 = new Scanner(System.in);
         System.out.print("Masukkan Angka : ");
         int angka = input29.nextInt();
+        String message = angka % 2 == 0 ? " bilangan genap" : " bilangan ganjil";
+        System.out.println("Angka "+angka+ message);
 
-    if (angka % 2 == 0)
-        System.out.println("Angka"+angka+" bilangan genap");
-    else 
-        System.out.println("Angka"+angka+" bilangan ganjil");
+    // if (angka % 2 == 0)
+    //     System.out.println("Angka "+angka+" bilangan genap");
+    // else 
+    //     System.out.println("Angka"+angka+" bilangan ganjil");
     }
     
 }
